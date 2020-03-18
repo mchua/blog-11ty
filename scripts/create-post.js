0de1f869934452dcf9b301fe56b69f74f5402196
David Peter <david.a.peter@gmail.com>
2020-03-17 19:21:51 -0700

    Create post now opens in the editor. Errors if no editor found.

--- scripts/create-post.js
+++ scripts/create-post.js
@@ -2,7 +2,9 @@ const readline = require("readline");
 const fs = require("fs");
 const path = require("path");
 const slugify = require("slugify");
+const openEditor = require("open-editor");
 
+const EDITOR_CMD = process.env.EDITOR || process.env.VISUAL || "vim";
 const BLOG_DIR = path.resolve(__dirname, "..", "src", "blog");
 
 const rl = readline.createInterface({
@@ -10,21 +12,16 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const question = "Hello, Mel. What would you like to write today?\ntitle> ";
-rl.question(question, title => {
-  const fileName = createFileName(title);
-  console.log(`Creating file: ${fileName}`);
-  fs.writeFileSync(
-    fileName,
-    `---
-title: ${title}
----`
-  );
-  console.log("File created!");
-  rl.close();
-});
+function ask(question) {
+  return new Promise(resolve => {
+    rl.question(question, answer => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
 
-const createFileName = title => {
+function createFileName(title) {
   // Don't need a lib for this, y'all.
   const leftpad = str => `00${str}`.slice(-2);
 
@@ -37,4 +34,42 @@ const createFileName = title => {
     lower: true
   });
   return `${BLOG_DIR}/${year}-${month}-${day}-${slug}.md`;
-};
+}
+
+(async () => {
+  let title;
+  try {
+    const question = "Hello, Mel. What would you like to write today?\ntitle> ";
+    title = await ask(question);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+  const fileName = createFileName(title);
+
+  try {
+    console.log(`Creating file: ${fileName}`);
+    fs.writeFileSync(
+      fileName,
+      `---
+title: ${title}
+---`,
+      { flag: "wx" }
+    );
+  } catch (e) {
+    if (e.code === "EEXIST") {
+      console.error("ERROR: File already exists!");
+    } else {
+      console.error(e);
+    }
+    process.exit(1);
+  }
+
+  try {
+    console.log(`File created! Opening with editor...`);
+    await openEditor([fileName]);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
